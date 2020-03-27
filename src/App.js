09ccc25060ff2f12a8e638e27b8d3145bf00bde3
unknown <zxcbnm6404@gmail.com>
2020-03-27 18:30:17 +0900

    라우터 리다이렉트까지

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { HashRouter, Route } from "react-router-dom";
+import About from "./routes/About";
+import Home from "./routes/Home";
+import Navigation from './components/Navigation'
+import Detail from './routes/Detail'
+
+//import Proptypes from "prop-types";
+
+// const foodIList = [
+//   { id: 1, name: "kimchi", rating: 4.5 },
+//   { id: 2, name: "banana", rating: 3.5 },
+//   { id: 3, name: "apple", rating: 5 }
+// ];
+
+// function Food({ name, rating }) {
+//   return (
+//     <div>
+//       <h1>{name} 굿굿</h1>
+//       <p>{rating}/5.0</p>
+//     </div>
+//   );
+// }
+
+// Food.propTypes = {
+//   name: Proptypes.string.isRequired,
+//   rating: Proptypes.number
+// };
+
+// function App() {
+//   return (
+//     <div>
+//       {foodIList.map(dish => (
+//         <Food key={dish.id} name={dish.name} rating={dish.rating} />
+//       ))}
+//     </div>
+//   );
+// }
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <Navigation/>
+      <Route path="/" exact={true} component={Home}></Route>
+      <Route path="/about" component={About}></Route>
+      <Route path="/movie/:id" component={Detail}/>
+    </HashRouter>
   );
 }
 
